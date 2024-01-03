1ac553ba32fbacb4345a69052e8dd829a7f3d470
FrankLloyd83 <ctesch58@gmail.com>
2024-01-03 10:27:55 +0100

    differentiated server and loger features, created getLogs route

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask, request
 import config
-from classes.Logger import Logger
+from classes.Logger import LoggerServer
 
 app = Flask(__name__)
 app.config.from_object(config)
-log = Logger(app.config["LOG_PATH"])
+log = LoggerServer(app.config["LOG_PATH"])
 
 @app.route("/", methods=["GET", "POST"])
 def default():
     data = request.data.decode("utf-8")
     log.log(data)
     return "Hello World!"
+
+@app.route("/getLogs", methods=["GET"])
+def getLogs():
+    return log.getLogs()
+
+@app.route("/checkConnection", methods=["GET"])
+def checkConnection():
+    return "Connection successful!"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
