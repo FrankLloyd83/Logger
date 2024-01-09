c94c9dabef3a92e135caaa5214850d922936f32c
FrankLloyd83 <ctesch58@gmail.com>
2024-01-09 11:28:02 +0100

    added token validation steps in routes accessed by external API, created Send route to send logging elements to server instance

--- app.py
+++ app.py
@@ -4,13 +4,20 @@ from Logger.classes.Logger import LoggerServer
 
 app = Flask(__name__)
 app.config.from_object(config)
-log = LoggerServer(app.config["LOG_PATH"])
+log = LoggerServer(app.config)
 
-@app.route("/", methods=["GET", "POST"])
-def default():
-    data = request.data.decode("utf-8")
-    log.log(data)
+
+@app.route("/")
+def index():
     return "Hello World!"
+
+@app.route("/Send", methods=["POST"])
+def send():
+    if log.validateAccessToken(request.headers.get("Authorization").split(" ")[1], 
+                               "https://login.microsoftonline.com/3200bc23-08a5-4747-b66c-167d2263eb17",
+                               log.client_id):
+        data = request.data.decode("utf-8")
+        log.log(data)
 
 @app.route("/getLogs", methods=["GET"])
 def getLogs():
@@ -18,7 +25,12 @@ def getLogs():
 
 @app.route("/checkConnection", methods=["GET"])
 def checkConnection():
-    return "Connection successful!"
+    if log.validateAccessToken(request.headers.get("Authorization").split(" ")[1], 
+                               "https://login.microsoftonline.com/3200bc23-08a5-4747-b66c-167d2263eb17",
+                               log.client_id):
+        return "Connection successful!"
+    else:
+        return "Connection failed!"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
