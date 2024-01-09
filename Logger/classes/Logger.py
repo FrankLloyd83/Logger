5f461301b7964e63d6cade38855e609eb5b17d4c
FrankLloyd83 <ctesch58@gmail.com>
2024-01-09 11:26:24 +0100

    added functions to get and validate access token to send requests to server instance

--- Logger/classes/Logger.py
+++ Logger/classes/Logger.py
@@ -6,14 +6,88 @@ import requests
 from requests.exceptions import ConnectionError
 import threading
 import time
+import jwt
+import json
 
 
 class Logger:
-    def __init__(self, path: str):
-        self.path = path
+    def __init__(self, config: dict):
+        self.path = os.getcwd() + "/" + config["LOG_PATH"]
+        self.tenant_id = config["TENANT_ID"]
+        self.client_id = config["CLIENT_ID"]
+        self.client_secret = config["CLIENT_SECRET"]
+        self.scope = config["SCOPE"]
         self.checkPath()
         self.patternNameFile = r"^log-(\d{8}).txt$"
         self.url = "https://olaps-logger.azurewebsites.net"
+    
+    def get_public_key(self, issuer: str, kid: str) -> jwt.algorithms.RSAAlgorithm:
+        """
+        Retrieves the public key from the authentication server.
+
+        Args:
+            issuer (str): The URL of the authentication server.
+            kid (str): The key ID of the public key to retrieve.
+
+        Returns:
+            RSAAlgorithm: The RSAAlgorithm object representing the retrieved public key.
+        """
+        jwks_url = issuer + f"/discovery/keys?appid={self.client_id}"
+        jwks = requests.get(jwks_url).json()
+        for key in jwks["keys"]:
+            if key["kid"] == kid:
+                return jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(key))
+
+    def validateAccessToken(self, token: str, issuer: str, client_id: str) -> bool:
+        """
+        Validates the given access token.
+
+        Args:
+            token (str): The access token to be validated.
+            issuer (str): The issuer of the access token.
+            client_id (str): The client ID of the access token.
+
+        Returns:
+            bool: True if the access token is valid, False otherwise.
+        """
+        header, payload, signature = token.split(".")
+
+        header = jwt.get_unverified_header(token)
+        kid = header["kid"]
+        public_key = self.get_public_key(issuer, kid)
+
+        options = {
+            "verify_signature": True,
+            "verify_exp": True,
+            "verify_iss": True,
+            "verify_aud": True,
+            "require": ["exp", "iss", "aud"],
+        }
+
+        try:
+            claims = jwt.decode(
+                token,
+                public_key,
+                algorithms=["RS256"],
+                audience="api://{}".format(client_id),
+                options=options,
+            )
+        except jwt.ExpiredSignatureError as e:
+            print(e)
+            return False
+        except jwt.InvalidIssuerError as e:
+            print(e)
+            return False
+        except jwt.InvalidAudienceError as e:
+            print(e)
+            return False
+        except jwt.InvalidTokenError as e:
+            print(e)
+            return False
+        except Exception as e:
+            print(e)
+            return False
+        return True
 
     def checkPath(self) -> None:
         """
@@ -114,6 +188,7 @@ class Logger:
         Retrieves logs within a specified date range.
 
         Args:
+            token (str): The access token to be validated.
             dateStart (datetime.date, optional): The start date of the range. Defaults to None.
             dateEnd (datetime.date, optional): The end date of the range. Defaults to None.
             desc (bool, optional): Specifies whether the logs should be sorted in descending order. Defaults to True.
@@ -198,8 +273,36 @@ class LoggerServer(Logger):
 
 
 class LoggerClient(Logger):
-    def __init__(self, path: str):
-        super().__init__(path)
+    def __init__(self, config: dict):
+        super().__init__(config)
+
+    def get_logger_token(self, token: str) -> str:
+        """
+        Retrieves the logger token from the authentication server.
+
+        Args:
+            token (str): The access token to be validated.
+
+        Returns:
+            str: The logger token.
+        """
+        try:
+            logger_token = requests.post(
+                "https://login.microsoftonline.com/3200bc23-08a5-4747-b66c-167d2263eb17/oauth2/v2.0/token",
+                data={
+                    "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+                    "client_id": self.client_id,
+                    "client_secret": self.client_secret,
+                    "scope": self.scope,
+                    "assertion": token,
+                    "requested_token_use": "on_behalf_of",
+                },
+                timeout=30,
+            ).json()
+            self.token = logger_token["access_token"]
+        except Exception as e:
+            print(e)
+            self.token = None
 
     def logToServer(self, messages: tuple) -> requests.Response or None:
         """
@@ -214,10 +317,11 @@ class LoggerClient(Logger):
         Raises:
             ConnectionError: If the request fails.
         """
-        headers = {"Content-Type": "application/json"}
+        url = f"{self.url}/Send"
+        headers = {"Content-Type": "application/json", "Authorization": f"Bearer {self.token}"}
         data = self.formatMessage(messages)
         try:
-            response = requests.post(self.url, data=data, headers=headers)
+            response = requests.post(url, data=data, headers=headers)
         except ConnectionError as e:
             raise e
 
@@ -267,7 +371,7 @@ class LoggerClient(Logger):
             time.sleep(5)
         buffer_content = "\n".join(buffer)
         headers = {"Content-Type": "application/json"}
-        url = f"{self.url}/checkConnection"
+        url = f"{self.url}/Send"
         try:
             response = requests.post(url, data=buffer_content, headers=headers)
         except ConnectionError as e:
@@ -281,11 +385,12 @@ class LoggerClient(Logger):
             raise Exception(
                 f"Could not empty buffer. Status code: {response.status_code}"
             )
-        
+
     def checkConnectionToServer(self) -> None:
         url = f"{self.url}/checkConnection"
+        headers = {"Authorization": f"Bearer {self.token}"}
         try:
-            response = requests.get(url)
+            response = requests.get(url, headers=headers, timeout=30)
         except ConnectionError as e:
             return False
         if response.status_code == 200:
@@ -294,4 +399,3 @@ class LoggerClient(Logger):
     def createThread(self):
         thread = threading.Thread(target=self.emptyBuffer, args=(self.getLogs(),))
         thread.start()
-
