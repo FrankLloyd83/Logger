e68c68737095773ce9cf40317124b62833b9ea60
FrankLloyd83 <ctesch58@gmail.com>
2024-01-11 17:27:47 +0100

    added default values for config fields

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 LOG_PATH = os.environ.get("LOG_PATH", "logs")
-TENANT_ID = os.environ.get("TENANT_ID")
+TENANT_ID = os.environ.get("TENANT_ID", "common")
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 LOG_SCOPE = os.environ.get("LOG_SCOPE")
-URL_ISSUER_BASE = os.environ.get("URL_ISSUER_BASE")
+URL_ISSUER_BASE = os.environ.get("URL_ISSUER_BASE", "https://login.microsoftonline.com/")
